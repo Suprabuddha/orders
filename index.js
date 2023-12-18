ed53e964b491c68ede8e65bef8679bf5456b3060
EC2 Default User <ec2-user@ip-10-0-1-208.ap-south-1.compute.internal>
2023-12-18 05:50:47 +0000

    Aws xray sdk code added

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
+const awsxray = require("aws-xray-sdk");
+
 const app = express();
 app.use(express.json());
 const { port } = require("./config");
 const PORT = process.env.PORT || port;
 
-app.listen(PORT, () => {
-  console.log("Server Listening on PORT:", PORT);
-});
+awsxray.captureHTTPsGlobal(require('http'), true);
+
+
 
 app.get('/status', (request, response) => {
    const status = {
       'Status': 'Running'
    };
-   
+   response.status(200);
    response.send(status);
+});
+
+app.listen(PORT, () => {
+  console.log("Server Listening on PORT:", PORT);
 });
